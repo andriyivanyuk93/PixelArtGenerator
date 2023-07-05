ed8e3cf2b93cfa959337152ac48ec159a022643b
andriyivanyuk93 <136633208+andriyivanyuk93@users.noreply.github.com>
2023-07-05 17:20:11 +0200

    Update script.js
    Update the JavaScript code to include functionality for customizing pixel sizes and saving and sharing generated art.

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // JavaScript code for the pixel art generator
 const canvas = document.getElementById('pixel-canvas');
 const generateButton = document.getElementById('generate-button');
+const saveButton = document.getElementById('save-button');
+const shareButton = document.getElementById('share-button');
+const pixelSizeInput = document.getElementById('pixel-size');
 
-// Add event listener to generate button
+// Add event listeners
 generateButton.addEventListener('click', generatePixelArt);
+saveButton.addEventListener('click', savePixelArt);
+shareButton.addEventListener('click', sharePixelArt);
 
 function generatePixelArt() {
-  // Get canvas context and set dimensions
-  const ctx = canvas.getContext('2d');
+  const pixelSize = parseInt(pixelSizeInput.value);
   const width = 200;
   const height = 200;
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = width * pixelSize;
+  canvas.height = height * pixelSize;
+  const ctx = canvas.getContext('2d');
 
   // Generate random pixel colors
   for (let x = 0; x < width; x++) {
@@ -20,7 +25,22 @@ function generatePixelArt() {
       const g = Math.floor(Math.random() * 256);
       const b = Math.floor(Math.random() * 256);
       ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-      ctx.fillRect(x, y, 1, 1);
+      ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
     }
   }
+}
+
+function savePixelArt() {
+  // Get the canvas image data and create a temporary link to download
+  const link = document.createElement('a');
+  link.href = canvas.toDataURL();
+  link.download = 'pixel-art.png';
+  link.click();
+}
+
+function sharePixelArt() {
+  // Get the canvas image data and open a new window to share on social media
+  const imageData = canvas.toDataURL();
+  const shareURL = `https://example.com/share?image=${encodeURIComponent(imageData)}`;
+  window.open(shareURL, '_blank');
 }
